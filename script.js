15d22169209e2e4ca62d013650321e563e033b95
ojasvijain1 <ojasvijainfeb4@gmail.com>
2024-01-07 20:43:22 +0530

    Calculating solution

--- script.js
+++ script.js
@@ -1,6 +1,46 @@
 console.log("Let's write some JavaScript");
 let isOperationUsed = false;
 let num = "";
+let storedHTML = "";
+
+operations = {
+    "plus": '+',
+    "subtraction": '-',
+    "divide": '/',
+    "multiply": '*',
+    "percent": "%",
+}
+
+function calculation(HTML) {
+    let num1, num2, sol;
+    
+    let srcMatch = HTML.match(/src="([^"]+)"/);
+    let srcAttributeValue;
+    if (srcMatch) {
+        srcAttributeValue = srcMatch[1];
+    }
+    
+    num1 = parseInt(HTML.split(`<img src="${srcAttributeValue}">`)[0])
+    num2 = parseInt(HTML.split(`<img src="${srcAttributeValue}">`)[1])
+    operator = operations[srcAttributeValue.split(".")[0]];
+    switch(operator) {
+        case '+': sol = num1 + num2;
+                  document.querySelector(".solution").innerHTML = sol;
+                  return true;
+        case '-': sol = num1 - num2;
+                  document.querySelector(".solution").innerHTML = sol;
+                  return true;
+        case '/': sol = num1 / num2;
+                  document.querySelector(".solution").innerHTML = sol;
+                  return true;
+        case '*': sol = num1 * num2;
+                  document.querySelector(".solution").innerHTML = sol;
+                  return true;
+        case '%': sol = (num1 * num2) / 100;
+                  document.querySelector(".solution").innerHTML = sol;
+                  return true;
+    }
+}
 
 document.querySelectorAll(".operation").forEach(button => {
     button.addEventListener("click", async e => {
@@ -14,17 +54,31 @@ document.querySelectorAll(".operation").forEach(button => {
         isOperationUsed = true;
         if (document.querySelector(".numbers").innerHTML.trim() == "") {
             alert("Cannot use any operation right now");
+            isOperationUsed = false;
         } else {
-            document.querySelector(".numbers").innerHTML = document.querySelector(".numbers").innerHTML + `<img src=${srcAttributeValue}>`;
+            if (srcAttributeValue == "equal.svg") {
+                calculation(document.querySelector(".numbers").innerHTML);
+            }
+            else {
+                document.querySelector(".numbers").innerHTML = document.querySelector(".numbers").innerHTML + `<img src=${srcAttributeValue}>`;
+                num = "";
+                storedHTML = document.querySelector(".numbers").innerHTML;
+            }
         }
         await logIsOperationUsed();
     });
 });
 
 document.querySelectorAll(".number").forEach(button => {
-    button.addEventListener("click", e => {
+    button.addEventListener("click", async e => {
         num += e.currentTarget.innerHTML;
-        document.querySelector(".numbers").innerHTML = num;
+        let a = await logIsOperationUsed();
+        if (a == true) {
+            document.querySelector(".numbers").innerHTML = storedHTML + num;
+        }
+        else {
+            document.querySelector(".numbers").innerHTML = num;
+        }
     })
 })
 
@@ -32,12 +86,14 @@ document.querySelector(".cancel").addEventListener("click", async () => {
     document.querySelector(".numbers").innerHTML = "";
     document.querySelector(".solution").innerHTML = "";
     num = "";
+    storedHTML = "";
     isOperationUsed = false;
     await logIsOperationUsed();
 })
 
 async function logIsOperationUsed() {
     console.log(isOperationUsed);
+    return isOperationUsed;
 }
 
 // This will be executed immediately after the event listeners are attached
